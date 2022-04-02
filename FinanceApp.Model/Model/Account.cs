f3707e7acd4caf9eb3921243fb6c1139b7512b1d
Brian Ahern <aherntb@gmail.com>
2022-04-02 19:01:39 +0100

    Add basic code to Account class and changed to be abstract

--- FinanceApp.Model/Model/Account.cs
+++ FinanceApp.Model/Model/Account.cs
@@ -1,6 +1,38 @@
 namespace FinanceApp.Model;
 
-public class Account
+public abstract class Account
 {
+    //Private balance variable. Can't be set from outside the class
+    private decimal _balance;
 
+    // Private transaction collection
+    private IList<Transaction> _transacions;
+
+    //Public property representing Account name
+    public string Name { get; private set; }
+
+    public Account(string name)
+    {
+        Name = name;
+        _balance = 0m;
+        _transacions = new List<Transaction>();
+    }
+
+    /*
+    * Returns the current balance of the account
+    */
+    public decimal GetCurrentBalance()
+    {
+        return _balance < 0m ? 0m : _balance;
+    }
+
+    /*
+    * Get a collection of transactions that were processed for the account
+    * virtual: can be overriden in derived classes to provide specific implementations related to specific accounts
+    */
+
+    public virtual IList<Transaction> GetAllTransactions()
+    {
+        return _transacions;
+    }
 }
